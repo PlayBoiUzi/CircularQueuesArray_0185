5aaad684956ad50f1f4dc7860f7d0c3b318042ec
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-06-12 11:27:14 +0700

    mengecek apakah antrian kosong

--- QueueArray.cpp
+++ QueueArray.cpp
@@ -50,4 +50,11 @@ class Queues
     }
    // Membuat fungsi untuk menghapus elemen dari antrian
     void remove()
-    
+    {
+        //cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        
