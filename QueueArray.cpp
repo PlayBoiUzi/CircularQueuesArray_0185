124539ddb632e1e4d6e84d1836c9a883c88293af
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-06-12 11:29:03 +0700

    iterasi dari awal array hingga REAR

--- QueueArray.cpp
+++ QueueArray.cpp
@@ -111,4 +111,13 @@ class Queues
 
             FRONT_position = 0;
 
-            
+            //iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+};
