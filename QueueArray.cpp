bd489b736b09073b4662564d67cfc15587bb1da9
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-06-12 11:23:45 +0700

    Kelas antrian menggunakan array

--- QueueArray.cpp
+++ QueueArray.cpp
@@ -4,3 +4,10 @@ using namespace std;
 // Membuat kelas antrian
 class Queues 
 {
+// Kelas antrian menggunakan array
+    private:
+    static const int max = 5;
+    int FRONT, REAR;
+    int queue_array[5];
+   
+    
